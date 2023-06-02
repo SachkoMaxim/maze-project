a28f90c0cb68a53775a594c10cd3ffd3b90a394e
unknown <sachko.maxim@lll.kpi.ua>
2023-06-02 18:26:26 +0300

    ended creating makeMaze function

--- maze/script.js
+++ maze/script.js
@@ -511,5 +511,17 @@ window.onresize = function() {
 };
 
 function makeMaze() {
-    
+    if (player != undefined) {
+        player.unbindKeyDown();
+        player = null;
+    }
+    const e = document.getElementById("diffSelect");
+    difficulty = e.options[e.selectedIndex].value;
+    cellSize = mazeCanvas.width / difficulty;
+    maze = new Maze(difficulty, difficulty);
+    draw = new DrawMaze(maze, ctx, cellSize, finishSprite);
+    player = new Player(maze, mazeCanvas, cellSize, displayVictoryMess, sprite);
+    if (document.getElementById("mazeContainer").style.opacity < "100") {
+        document.getElementById("mazeContainer").style.opacity = "100";
+    }
 }
