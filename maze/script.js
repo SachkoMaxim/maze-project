1925622e148ef6971f135c2901b611348c7bbdab
unknown <sachko.maxim@lll.kpi.ua>
2023-06-06 18:06:49 +0300

    changed DrawMaze function to arrow function

--- maze/script.js
+++ maze/script.js
@@ -177,116 +177,120 @@ const Maze = (Width, Height) => {
     };
 };
 
-function DrawMaze(Maze, ctx, cellSized, endSprite = null) {
+const DrawMaze = (Maze, ctx, cellSize, endSprite = null) => {
     const map = Maze.map();
-    let cellSize = cellSized;
-    let drawEndMethod;
     ctx.lineWidth = cellSize / 40;
-
-    this.redrawMaze = function(size) {
-        cellSize = size;
-        ctx.lineWidth = cellSize / 50;
-        drawMap();
-        drawEndMethod();
+  
+    const redrawMaze = (size) => {
+      cellSize = size;
+      ctx.lineWidth = cellSize / 50;
+      drawMap();
+      drawEndMethod();
     };
-
-    function drawCell(xCord, yCord, cell) {
-        let x = xCord * cellSize;
-        let y = yCord * cellSize;
-
-        if (cell.n == false) {
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-            ctx.lineTo(x + cellSize, y);
-            ctx.stroke();
+  
+    const drawCell = (xCord, yCord, cell) => {
+      const x = xCord * cellSize;
+      const y = yCord * cellSize;
+  
+      if (cell.n === false) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + cellSize, y);
+        ctx.stroke();
+      }
+      if (cell.s === false) {
+        ctx.beginPath();
+        ctx.moveTo(x, y + cellSize);
+        ctx.lineTo(x + cellSize, y + cellSize);
+        ctx.stroke();
+      }
+      if (cell.e === false) {
+        ctx.beginPath();
+        ctx.moveTo(x + cellSize, y);
+        ctx.lineTo(x + cellSize, y + cellSize);
+        ctx.stroke();
+      }
+      if (cell.w === false) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y + cellSize);
+        ctx.stroke();
+      }
+    };
+  
+    const drawMap = () => {
+      for (let x = 0; x < map.length; x++) {
+        for (let y = 0; y < map[x].length; y++) {
+          drawCell(x, y, map[x][y]);
         }
-        if (cell.s === false) {
-            ctx.beginPath();
-            ctx.moveTo(x, y + cellSize);
-            ctx.lineTo(x + cellSize, y + cellSize);
-            ctx.stroke();
+      }
+    };
+  
+    const drawEndFlag = () => {
+      const coord = Maze.finishCoord();
+      const gridSize = 4;
+      const fraction = cellSize / gridSize - 2;
+      let colorSwap = true;
+      for (let y = 0; y < gridSize; y++) {
+        if (gridSize % 2 === 0) {
+          colorSwap = !colorSwap;
         }
-        if (cell.e === false) {
-            ctx.beginPath();
-            ctx.moveTo(x + cellSize, y);
-            ctx.lineTo(x + cellSize, y + cellSize);
-            ctx.stroke();
+        for (let x = 0; x < gridSize; x++) {
+          ctx.beginPath();
+          ctx.rect(
+            coord.x * cellSize + x * fraction + 4.5,
+            coord.y * cellSize + y * fraction + 4.5,
+            fraction,
+            fraction
+          );
+          if (colorSwap) {
+            ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+          } else {
+            ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+          }
+          ctx.fill();
+          colorSwap = !colorSwap;
         }
-        if (cell.w === false) {
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-            ctx.lineTo(x, y + cellSize);
-            ctx.stroke();
-        }
-    }
-
-    function drawMap() {
-        for (let x = 0; x < map.length; x++) {
-            for (let y = 0; y < map[x].length; y++) {
-                drawCell(x, y, map[x][y]);
-            }
-        }
+      }
+    };
+  
+    const drawEndSprite = () => {
+      const offsetLeft = cellSize / 50;
+      const offsetRight = cellSize / 25;
+      const coord = Maze.finishCoord();
+      ctx.drawImage(
+        endSprite,
+        2,
+        2,
+        endSprite.width,
+        endSprite.height,
+        coord.x * cellSize + offsetLeft,
+        coord.y * cellSize + offsetLeft,
+        cellSize - offsetRight,
+        cellSize - offsetRight
+      );
+    };
+  
+    const clear = () => {
+      const canvasSize = cellSize * map.length;
+      ctx.clearRect(0, 0, canvasSize, canvasSize);
+    };
+  
+    let drawEndMethod;
+    if (endSprite != null) {
+      drawEndMethod = drawEndSprite;
+    } else {
+      drawEndMethod = drawEndFlag;
     }
   
-    function drawEndFlag() {
-        let coord = Maze.endCoord();
-        let gridSize = 4;
-        let fraction = cellSize / gridSize - 2;
-        let colorSwap = true;
-        for (let y = 0; y < gridSize; y++) {
-            if (gridSize % 2 == 0) {
-                colorSwap = !colorSwap;
-            }
-            for (let x = 0; x < gridSize; x++) {
-                ctx.beginPath();
-                ctx.rect(
-                    coord.x * cellSize + x * fraction + 4.5,
-                    coord.y * cellSize + y * fraction + 4.5,
-                    fraction,
-                    fraction
-                );
-                if (colorSwap) {
-                    ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
-                } else {
-                    ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-                }
-                ctx.fill();
-                colorSwap = !colorSwap;
-            }
-        }
-    }
-
-    function drawEndSprite() {
-        let offsetLeft = cellSize / 50;
-        let offsetRight = cellSize / 25;
-        let coord = Maze.endCoord();
-        ctx.drawImage(
-            endSprite,
-            2,
-            2,
-            endSprite.width,
-            endSprite.height,
-            coord.x * cellSize + offsetLeft,
-            coord.y * cellSize + offsetLeft,
-            cellSize - offsetRight,
-            cellSize - offsetRight
-        );
-    }
-    
-    function clear() {
-        const canvasSize = cellSize * map.length;
-        ctx.clearRect(0, 0, canvasSize, canvasSize);
-    }
-    
-    if (endSprite != null) {
-        drawEndMethod = drawEndSprite;
-    } else {
-        drawEndMethod = drawEndFlag;
-    }
     clear();
     drawMap();
     drawEndMethod();
-}
+  
+    return {
+      redrawMaze
+    };
+};
 
 const Player = (maze, canvas, _cellsize, onComplete, sprite = null) => {
     const ctx = canvas.getContext("2d");
