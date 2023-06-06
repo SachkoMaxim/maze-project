55094d527b92e214fbf3752f2288fe47dde1d016
unknown <sachko.maxim@lll.kpi.ua>
2023-06-06 16:46:20 +0300

    changed some functions to arrow functions

--- maze/script.js
+++ maze/script.js
@@ -1,14 +1,12 @@
-function rand(max) {
-    return Math.floor(Math.random() * max);
-}
+const rand = (max) => Math.floor(Math.random() * max);
 
- function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--){
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
- }
+const shuffle = (a) => {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+};
  
  function changeBrightness(factor, sprite) {
     const virtCanvas = document.createElement("canvas");
@@ -45,139 +43,139 @@ function rand(max) {
     }
  }
 
- function Maze(Width, Height) {
+ const Maze = (Width, Height) => {
     let mazeMap;
     const width = Width;
     const height = Height;
     let startCoord, endCoord;
     const dirs = ["n", "s", "e", "w"];
     const modDir = {
-        n: {
-            y: -1,
-            x: 0,
-            o: "s"
-        },
-        s: {
-            y: 1,
-            x: 0,
-            o: "n"
-        },
-        e: {
-            y: 0,
-            x: 1,
-            o: "w"
-        },
-        w: {
-            y: 0,
-            x: -1,
-            o: "e"
+      n: {
+        y: -1,
+        x: 0,
+        o: "s"
+      },
+      s: {
+        y: 1,
+        x: 0,
+        o: "n"
+      },
+      e: {
+        y: 0,
+        x: 1,
+        o: "w"
+      },
+      w: {
+        y: 0,
+        x: -1,
+        o: "e"
+      }
+    };
+  
+    const map = () => mazeMap;
+    const beginCoord = () => startCoord;
+    const finishCoord = () => endCoord;
+  
+    const genMap = () => {
+      mazeMap = new Array(height);
+      for (let y = 0; y < height; y++) {
+        mazeMap[y] = new Array(width);
+        for (let x = 0; x < width; ++x) {
+          mazeMap[y][x] = {
+            n: false,
+            s: false,
+            e: false,
+            w: false,
+            visited: false,
+            priorPos: null
+          };
         }
+      }
     };
-
-    this.map = function() {
-        return mazeMap;
-    };
-    this.startCoord = function() {
-        return startCoord;
-    };
-    this.endCoord = function() {
-        return endCoord;
-    };
-
-    function genMap() {
-        mazeMap = new Array(height);
-        for (let y = 0; y < height; y++) {
-            mazeMap[y] = new Array(width);
-            for (let x = 0; x < width; ++x) {
-                mazeMap[y][x] = {
-                    n: false,
-                    s: false,
-                    e: false,
-                    w: false,
-                    visited: false,
-                    priorPos: null
-                };
-            }
+  
+    const defineMaze = () => {
+      let isComp = false;
+      let move = false;
+      let cellsVisited = 1;
+      let numLoops = 0;
+      let maxLoops = 0;
+      let pos = {
+        x: 0,
+        y: 0
+      };
+      const numCells = width * height;
+      while (!isComp) {
+        move = false;
+        mazeMap[pos.x][pos.y].visited = true;
+  
+        if (numLoops >= maxLoops) {
+          shuffle(dirs);
+          maxLoops = Math.round(rand(height / 8));
+          numLoops = 0;
         }
-    }
-
-    function defineMaze() {
-        let isComp = false;
-        let move = false;
-        let cellsVisited = 1;
-        let numLoops = 0;
-        let maxLoops = 0;
-        let pos = {
-            x: 0,
-            y: 0
-        };
-        const numCells = width * height;
-        while (!isComp) {
-            move = false;
-            mazeMap[pos.x][pos.y].visited = true;
-
-            if (numLoops >= maxLoops) {
-                shuffle(dirs);
-                maxLoops = Math.round(rand(height / 8));
-                numLoops = 0;
+        numLoops++;
+        for (let index = 0; index < dirs.length; index++) {
+          const direction = dirs[index];
+          const nx = pos.x + modDir[direction].x;
+          const ny = pos.y + modDir[direction].y;
+  
+          if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+            //To check if the title is visited
+            if (!mazeMap[nx][ny].visited) {
+              //Carve through walls from this title to next
+              mazeMap[pos.x][pos.y][direction] = true;
+              mazeMap[nx][ny][modDir[direction].o] = true;
+  
+              //Set Currentcell as next cell's prior visited
+              mazeMap[nx][ny].priorPos = pos;
+              //Update Cell position to newly visited location
+              pos = {
+                x: nx,
+                y: ny
+              };
+              cellsVisited++;
+              //Recursively call this method on the next tile
+              move = true;
+              break;
             }
-            numLoops++;
-            for (let index = 0; index < dirs.length; index++) {
-                const direction = dirs[index];
-                const nx = pos.x + modDir[direction].x;
-                const ny = pos.y + modDir[direction].y;
-
-                if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
-                    //To check if the title is visited
-                    if (!mazeMap[nx][ny].visited) {
-                        //Carve through walls from this title to next
-                        mazeMap[pos.x][pos.y][direction] = true;
-                        mazeMap[nx][ny][modDir[direction].o] = true;
-
-                        //Set Currentcell as next cell's prior visited
-                        mazeMap[nx][ny].priorPos = pos;
-                        //Update Cell position to newly visited location
-                        pos = {
-                            x: nx,
-                            y: ny
-                        };
-                        cellsVisited++;
-                        //Recursively call this method on the next tile
-                        move = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!move) {
-                // If it failed to find a direction, move to the current position back to the prior cell and recall the method
-                pos = mazeMap[pos.x][pos.y].priorPos;
-            }
-            if (numCells === cellsVisited) {
-                isComp = true;
-            }
+          }
         }
-    }
-
-    function defineStartEnd() {
-        const positions = [
-            { startX: 0, startY: 0, endX: height - 1, endY: width - 1 },
-            { startX: 0, startY: width - 1, endX: height - 1, endY: 0 },
-            { startX: height - 1, startY: 0, endX: 0, endY: width - 1 },
-            { startX: height - 1, startY: width - 1, endX: 0, endY: 0 }
-        ];
-    
-        const randomIndex = Math.floor(Math.random() * positions.length);
-        const { startX, startY, endX, endY } = positions[randomIndex];
-    
-        startCoord = { x: startX, y: startY };
-        endCoord = { x: endX, y: endY };
-    }    
-
+  
+        if (!move) {
+          // If it failed to find a direction, move to the current position back to the prior cell and recall the method
+          pos = mazeMap[pos.x][pos.y].priorPos;
+        }
+        if (numCells === cellsVisited) {
+          isComp = true;
+        }
+      }
+    };
+  
+    const defineStartEnd = () => {
+      const positions = [
+        { startX: 0, startY: 0, endX: height - 1, endY: width - 1 },
+        { startX: 0, startY: width - 1, endX: height - 1, endY: 0 },
+        { startX: height - 1, startY: 0, endX: 0, endY: width - 1 },
+        { startX: height - 1, startY: width - 1, endX: 0, endY: 0 }
+      ];
+  
+      const randomIndex = Math.floor(Math.random() * positions.length);
+      const { startX, startY, endX, endY } = positions[randomIndex];
+  
+      startCoord = { x: startX, y: startY };
+      endCoord = { x: endX, y: endY };
+    };
+  
     genMap();
     defineStartEnd();
     defineMaze();
-}
+  
+    return {
+      map,
+      beginCoord,
+      finishCoord
+    };
+  };
 
 function DrawMaze(Maze, ctx, cellSized, endSprite = null) {
     const map = Maze.map();
@@ -290,8 +288,7 @@ function DrawMaze(Maze, ctx, cellSized, endSprite = null) {
     drawEndMethod();
 }
 
-function Player(maze, c, _cellsize, onComplete, sprite = null) {
-    const canvas = c;
+const Player = (maze, canvas, _cellsize, onComplete, sprite = null) => {
     const ctx = canvas.getContext("2d");
     let drawSprite;
     let moves = 0;
@@ -307,13 +304,13 @@ function Player(maze, c, _cellsize, onComplete, sprite = null) {
     };
     let cellSize = _cellsize;
     const halfCellSize = cellSize / 2;
-  
-    this.redrawPlayer = function (_cellsize) {
+
+    const redrawPlayer = (_cellsize) => {
         cellSize = _cellsize;
         drawSpriteImg(cellCoords);
     };
-  
-    function drawSpriteCircle(coord) {
+
+    const drawSpriteCircle = (coord) => {
         ctx.beginPath();
         ctx.fillStyle = "yellow";
         ctx.arc(
@@ -328,9 +325,9 @@ function Player(maze, c, _cellsize, onComplete, sprite = null) {
             onComplete(moves);
             player.unbindKeyDown();
         }
-    }
-  
-    function drawSpriteImg(coord) {
+    };
+
+    const drawSpriteImg = (coord) => {
         const offsetLeft = cellSize / 50;
         const offsetRight = cellSize / 25;
         ctx.drawImage(
@@ -348,9 +345,9 @@ function Player(maze, c, _cellsize, onComplete, sprite = null) {
             onComplete(moves);
             player.unbindKeyDown();
         }
-    }
-  
-    function removeSprite(coord) {
+    };
+
+    const removeSprite = (coord) => {
         const offsetLeft = cellSize / 50;
         const offsetRight = cellSize / 25;
         ctx.clearRect(
@@ -359,12 +356,12 @@ function Player(maze, c, _cellsize, onComplete, sprite = null) {
             cellSize - offsetRight,
             cellSize - offsetRight
         );
-    }
-  
-    function check(e) {
+    };
+
+    const check = (e) => {
         const cell = map[cellCoords.x][cellCoords.y];
         moves++;
-  
+
         const keyMap = {
             65: { prop: "w", x: -1, y: 0 },  // A or left arrow key
             37: { prop: "w", x: -1, y: 0 },  // A or left arrow key
@@ -375,10 +372,10 @@ function Player(maze, c, _cellsize, onComplete, sprite = null) {
             83: { prop: "s", x: 0, y: 1 },   // S or down arrow key
             40: { prop: "s", x: 0, y: 1 }    // S or down arrow key
         };
-  
+
         const keyCode = e.keyCode;
         const key = keyMap[keyCode];
-  
+
         if (key && cell[key.prop] === true) {
             removeSprite(cellCoords);
             cellCoords = {
@@ -387,24 +384,24 @@ function Player(maze, c, _cellsize, onComplete, sprite = null) {
             };
             drawSprite(cellCoords);
         }
-    }
-  
-    function handleTouchStart(event) {
+    };
+
+    const handleTouchStart = (event) => {
         xDown = event.touches[0].clientX;
         yDown = event.touches[0].clientY;
-    }
-  
-    function handleTouchMove(event) {
+    };
+
+    const handleTouchMove = (event) => {
         if (!xDown || !yDown) {
             return;
         }
-  
+
         const xUp = event.touches[0].clientX;
         const yUp = event.touches[0].clientY;
-  
+
         const xDiff = xDown - xUp;
         const yDiff = yDown - yUp;
-  
+
         if (Math.abs(xDiff) > Math.abs(yDiff)) {
             if (xDiff > 0) {
                 check({ keyCode: 37 }); // Left swipe
@@ -418,28 +415,34 @@ function Player(maze, c, _cellsize, onComplete, sprite = null) {
                 check({ keyCode: 40 }); // Down swipe
             }
         }
-  
+
         xDown = null;
         yDown = null;
-    }
-  
-    this.bindKeyDown = function () {
+    };
+
+    const bindKeyDown = () => {
         window.addEventListener("keydown", check, false);
-  
+
         canvas.addEventListener("touchstart", handleTouchStart, false);
         canvas.addEventListener("touchmove", handleTouchMove, false);
     };
-  
-    this.unbindKeyDown = function () {
+
+    const unbindKeyDown = () => {
         window.removeEventListener("keydown", check, false);
         canvas.removeEventListener("touchstart", handleTouchStart, false);
         canvas.removeEventListener("touchmove", handleTouchMove, false);
     };
-  
+
     drawSprite(maze.startCoord());
-  
-    this.bindKeyDown();
-}
+
+    bindKeyDown();
+
+    return {
+        redrawPlayer,
+        bindKeyDown,
+        unbindKeyDown
+    };
+};
 
 const mazeCanvas = document.getElementById("mazeCanvas");
 const ctx = mazeCanvas.getContext("2d");
