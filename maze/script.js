9d921bd8447b68f382d043acf994fa097008fe52
unknown <sachko.maxim@lll.kpi.ua>
2023-06-07 12:30:36 +0300

    Changed shuffle function to an arrow function

--- maze/script.js
+++ maze/script.js
@@ -1,12 +1,12 @@
 const rand = (max) => Math.floor(Math.random() * max);
 
-function shuffle(a) {
+const shuffle = (a) => {
   for (let i = a.length - 1; i > 0; i--){
       const j = Math.floor(Math.random() * (i + 1));
       [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
-}
+};
 
 function changeBrightness(factor, sprite) {
   const virtCanvas = document.createElement("canvas");
