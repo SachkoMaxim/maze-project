a04a01eaf703f990d2b2d62766b81e549c42573f
unknown <sachko.maxim@lll.kpi.ua>
2023-06-07 16:53:17 +0300

    Changed transparency of the colors

--- maze/script.js
+++ maze/script.js
@@ -243,9 +243,9 @@ function drawMaze(labyrinth, ctx, cellSized, endSprite = null) {
                   fraction
               );
               if (colorSwap) {
-                  ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
+                  ctx.fillStyle = "rgba(0, 0, 0, 1.5)";
               } else {
-                  ctx.fillStyle = "rgba(255, 255, 255, 1.5)";
+                  ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
               }
               ctx.fill();
               colorSwap = !colorSwap;
