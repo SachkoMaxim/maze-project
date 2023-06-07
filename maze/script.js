d85cdc1541239ca5d00da2c0e37e1b8f31916e0d
unknown <sachko.maxim@lll.kpi.ua>
2023-06-07 17:37:28 +0300

    Returned drawMaze function

--- maze/script.js
+++ maze/script.js
@@ -175,88 +175,115 @@ function maze (width, height) {
   defineMaze();
 }
 
-function drawMaze(labyrinth, ctx, cellSize, endSprite = null) {
+function drawMaze(labyrinth, ctx, cellSized, endSprite = null) {
   const map = labyrinth.map();
-  const canvasSize = cellSize * map.length;
+  let cellSize = cellSized;
+  let drawEndMethod;
+  ctx.lineWidth = cellSize / 30;
 
-  this.redrawMaze = (newCellSize) => {
-    cellSize = newCellSize;
-    clear();
-    drawMap();
-    drawEnd();
+  this.redrawMaze = (size) => {
+      cellSize = size;
+      ctx.lineWidth = cellSize / 30;
+      drawMap();
+      drawEndMethod();
   };
 
   const drawCell = (xCord, yCord, cell) => {
-    const x = xCord * cellSize;
-    const y = yCord * cellSize;
-
-    ctx.beginPath();
-    ctx.lineWidth = cellSize / 30;
-
-    if (!cell.n) {
-      ctx.moveTo(x, y);
-      ctx.lineTo(x + cellSize, y);
-    }
-    if (!cell.s) {
-      ctx.moveTo(x, y + cellSize);
-      ctx.lineTo(x + cellSize, y + cellSize);
-    }
-    if (!cell.e) {
-      ctx.moveTo(x + cellSize, y);
-      ctx.lineTo(x + cellSize, y + cellSize);
-    }
-    if (!cell.w) {
-      ctx.moveTo(x, y);
-      ctx.lineTo(x, y + cellSize);
-    }
-
-    ctx.stroke();
+      let x = xCord * cellSize;
+      let y = yCord * cellSize;
+      if (cell.n == false) {
+          ctx.beginPath();
+          ctx.moveTo(x, y);
+          ctx.lineTo(x + cellSize, y);
+          ctx.stroke();
+      }
+      if (cell.s === false) {
+          ctx.beginPath();
+          ctx.moveTo(x, y + cellSize);
+          ctx.lineTo(x + cellSize, y + cellSize);
+          ctx.stroke();
+      }
+      if (cell.e === false) {
+          ctx.beginPath();
+          ctx.moveTo(x + cellSize, y);
+          ctx.lineTo(x + cellSize, y + cellSize);
+          ctx.stroke();
+      }
+      if (cell.w === false) {
+          ctx.beginPath();
+          ctx.moveTo(x, y);
+          ctx.lineTo(x, y + cellSize);
+          ctx.stroke();
+      }
   };
 
   const drawMap = () => {
-    for (let x = 0; x < map.length; x++) {
-      for (let y = 0; y < map[x].length; y++) {
-        drawCell(x, y, map[x][y]);
+      for (let x = 0; x < map.length; x++) {
+          for (let y = 0; y < map[x].length; y++) {
+              drawCell(x, y, map[x][y]);
+          }
       }
-    }
   };
 
-  const drawEnd = () => {
-    const coord = labyrinth.endCoord();
-    const gridSize = 4;
-    const fraction = cellSize / gridSize - 2;
-    let colorSwap = true;
-
-    for (let y = 0; y < gridSize; y++) {
-      if (gridSize % 2 === 0) {
-        colorSwap = !colorSwap;
+  const drawEndFlag = () => {
+      let coord = labyrinth.endCoord();
+      let gridSize = 4;
+      let fraction = cellSize / gridSize - 2;
+      let colorSwap = true;
+      for (let y = 0; y < gridSize; y++) {
+          if (gridSize % 2 == 0) {
+              colorSwap = !colorSwap;
+          }
+          for (let x = 0; x < gridSize; x++) {
+              ctx.beginPath();
+              ctx.rect(
+                  coord.x * cellSize + x * fraction + 4.5,
+                  coord.y * cellSize + y * fraction + 4.5,
+                  fraction,
+                  fraction
+              );
+              if (colorSwap) {
+                  ctx.fillStyle = "rgba(0, 0, 0, 1.5)";
+              } else {
+                  ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+              }
+              ctx.fill();
+              colorSwap = !colorSwap;
+          }
       }
-      for (let x = 0; x < gridSize; x++) {
-        ctx.beginPath();
-        ctx.rect(
-          coord.x * cellSize + x * fraction + 4.5,
-          coord.y * cellSize + y * fraction + 4.5,
-          fraction,
-          fraction
-        );
-        if (colorSwap) {
-          ctx.fillStyle = "rgba(0, 0, 0, 1.5)";
-        } else {
-          ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
-        }
-        ctx.fill();
-        colorSwap = !colorSwap;
-      }
-    }
   };
 
+  const drawEndSprite = () => {
+      let offsetLeft = cellSize / 50;
+      let offsetRight = cellSize / 25;
+      let coord = labyrinth.endCoord();
+      ctx.drawImage(
+          endSprite,
+          2,
+          2,
+          endSprite.width,
+          endSprite.height,
+          coord.x * cellSize + offsetLeft,
+          coord.y * cellSize + offsetLeft,
+          cellSize - offsetRight,
+          cellSize - offsetRight
+      );
+  };
+  
   const clear = () => {
-    ctx.clearRect(0, 0, canvasSize, canvasSize);
+      const canvasSize = cellSize * map.length;
+      ctx.clearRect(0, 0, canvasSize, canvasSize);
   };
+  
+  if (endSprite != null) {
+      drawEndMethod = drawEndSprite;
+  } else {
+      drawEndMethod = drawEndFlag;
+  }
 
   clear();
   drawMap();
-  drawEnd();
+  drawEndMethod();
 }
 
 function player(labyrinth, canvas, cellSized, onComplete, sprite = null) {
