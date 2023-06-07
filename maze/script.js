24ebf8309f478477d2121aed21fc9b43adba53a9
unknown <sachko.maxim@lll.kpi.ua>
2023-06-07 17:49:34 +0300

    Changed drawCell function

--- maze/script.js
+++ maze/script.js
@@ -192,28 +192,16 @@ function drawMaze(labyrinth, ctx, cellSized, endSprite = null) {
       let x = xCord * cellSize;
       let y = yCord * cellSize;
       if (cell.n == false) {
-          ctx.beginPath();
-          ctx.moveTo(x, y);
-          ctx.lineTo(x + cellSize, y);
-          ctx.stroke();
+        ctx.fillRect(x, y, cellSize, ctx.lineWidth);
       }
       if (cell.s === false) {
-          ctx.beginPath();
-          ctx.moveTo(x, y + cellSize);
-          ctx.lineTo(x + cellSize, y + cellSize);
-          ctx.stroke();
+        ctx.fillRect(x, y + cellSize - ctx.lineWidth, cellSize, ctx.lineWidth);
       }
       if (cell.e === false) {
-          ctx.beginPath();
-          ctx.moveTo(x + cellSize, y);
-          ctx.lineTo(x + cellSize, y + cellSize);
-          ctx.stroke();
+        ctx.fillRect(x + cellSize - ctx.lineWidth, y, ctx.lineWidth, cellSize);
       }
       if (cell.w === false) {
-          ctx.beginPath();
-          ctx.moveTo(x, y);
-          ctx.lineTo(x, y + cellSize);
-          ctx.stroke();
+        ctx.fillRect(x, y, ctx.lineWidth, cellSize);
       }
   };
 
